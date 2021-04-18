43c826ca4a3e170954ff66eca45c8347f28db31a
Hari Udayakumar <hariudkmr@gmail.com>
2021-04-18 23:51:20 +0800

    added script for installing the required libraries

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Author : Hari Udayakumar
+# Script follows here: Required Libaries
+
+sudo apt install libglm-dev
+sudo apt-get install libsdl2-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-gfx-dev
+
+echo "Libararies Installed"
